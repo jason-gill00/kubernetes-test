b9a99c6349131396cc56ea3067f602df4babbaca
Jason Gill <jasongill2000@hotmail.com>
2025-04-16 09:48:11 -0400

    wip

--- src/temporal/worker.ts
+++ src/temporal/worker.ts
@@ -5,12 +5,10 @@ import * as fs from 'fs';
 const run = async () => {
   console.log("RUNNING WORKER")
   // set timeout to an hour
-  const timeout = 60 * 60 * 1000;
-  await new Promise((resolve) => setTimeout(resolve, timeout));
-  //console.log({
-  //  address: process.env.TEMPORAL_CLOUD_ADDRESS,
-  //  apiKey: process.env.TEMPORAL_CLOUD_API_KEY,
-  //})
+  console.log({
+    address: process.env.TEMPORAL_CLOUD_ADDRESS,
+    apiKey: process.env.TEMPORAL_CLOUD_API_KEY,
+  })
   const connection = process.env.NODE_ENV === 'production' ?
     {
       connection: await NativeConnection.connect({
@@ -28,23 +26,23 @@ const run = async () => {
     ...connection,
   });
 
-  //
-  //const version = process.env.VERSION || 'unknown';
-  //
-  //const productWorker = await Worker.create({
-  //  workflowsPath: require.resolve('./workflows'),
-  //  activities,
-  //  taskQueue: `product-queue-${version}`,
-  //  ...connection,
-  //});
-  //
-  //
-  //const entityWorkerRunPromise = entityWorker.run();
-  //const productWorkerRunPromise = productWorker.run();
-  //
-  //console.log('Workers started');
-  //
-  //await Promise.all([entityWorkerRunPromise, productWorkerRunPromise]);
+
+  const version = process.env.VERSION || 'unknown';
+
+  const productWorker = await Worker.create({
+    workflowsPath: require.resolve('./workflows'),
+    activities,
+    taskQueue: `product-queue-${version}`,
+    ...connection,
+  });
+
+
+  const entityWorkerRunPromise = entityWorker.run();
+  const productWorkerRunPromise = productWorker.run();
+
+  console.log('Workers started');
+
+  await Promise.all([entityWorkerRunPromise, productWorkerRunPromise]);
 }
 
 run().catch((err) => {
